a9cf90bc3cae1653dc4e1b1bb956812301bae078
phdun <phdun@T>
2025-12-03 22:10:57 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MedRecordsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
